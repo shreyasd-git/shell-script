fe8e0e5c0e02ea77bf20ce4308569346f38adb39
shreyasd-git <shreyasdevarkar007@gmail.com>
2025-10-10 19:36:08 +0530

    cleaned repo

--- ram_usage_by_service.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Monitor system processes and their memory usage
-
-# --sort=-%mem > sorts in desceding order for memory %
-# head -n 10 > gives top 10 results
-ps aux --sort=-%mem | head -n 10
